e15353a78e8b8e4cb4af67ef65cdefd9925385fb
Essi Monneus <essimonneus@gmail.com>
2021-09-20 14:38:00 -0400

    new challenge

---
+++ Arrays/Easy/reverseInteger.js
@@ -0,0 +1,7 @@
+var reverse = function(x) {
+    const number = Math.abs(x).toString().split('').reverse().join('');
+        if (number > 2**31) {
+                return 0
+        }
+    return x < 0 ? -number : number
+    };
